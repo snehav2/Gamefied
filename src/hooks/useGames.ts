c61ac66bf3fb3eecbb2b5e118875fc93e1fe638d
Sneha Verma <snehaverma0201@gmail.com>
2024-06-21 09:21:44 +0530

    Show Loading Skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,6 +12,7 @@ export interface Game {
     name: string;
     background_image:string;
     parent_platforms:{platform:Platform}[]
+    metacritic:number;
   }
   
   interface FetchGamesResponse {
@@ -21,19 +22,25 @@ export interface Game {
 const useGames=()=>{
     const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading,setLoading]=useState(false)
+
 
   useEffect(() => {
     const controller=new AbortController();
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games",{signal:controller.signal})
-      .then((res) => setGames(res.data.results))
+      .then((res) =>{setGames(res.data.results);
+                        setLoading(false);
+    })
       .catch((err) => {
             if (err instanceof CanceledError) return;
             setError(err.message)
+            setLoading(false)
   });
       
     return ()=>controller.abort();
   }, []);
-  return {games,error};
+  return {games,error,isLoading};
 }
 export default useGames;
