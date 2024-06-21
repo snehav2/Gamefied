324973fd8ece8214e32c63ac43c7790bada1b6af
Sneha Verma <snehaverma0201@gmail.com>
2024-06-21 11:31:10 +0530

    Display genres with text and image

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -9,7 +9,7 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{sm:1,md:2,lg:3,xl:5}} padding='10px' spacing={10}>
+      <SimpleGrid columns={{sm:1,md:2,lg:3,xl:5}} padding='10px' spacing={3}>
         {isLoading && skeletons.map(skeleton=><GameCardContainer><GameCardSkeleton key={skeleton}/></GameCardContainer>)}
         {data.map((game) => (
             <GameCardContainer>
