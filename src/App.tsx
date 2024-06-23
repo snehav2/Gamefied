c567790e196400c31ad53b483eed0586bac15deb
Sneha Verma <snehaverma0201@gmail.com>
2024-06-23 13:51:47 +0530

    filtering games by genre

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import {Grid, GridItem,Show} from "@chakra-ui/react"
 import NavBar from "./components/NavBar"
 import GameGrid from "./components/GameGrid"
 import GenreList from "./components/GenreList"
+import { useState } from "react";
+import { Genre } from "./hooks/useGenres";
 
 function App() {
-  
+   const [selectedGenre,setSelectedGenre]=useState<Genre|null>(null);
 
   return (
     <Grid templateAreas={{
@@ -18,10 +20,10 @@ function App() {
         <NavBar/>
         </GridItem>
       <Show above="lg">
-        <GridItem area="aside" paddingX={5}><GenreList/></GridItem>
+        <GridItem area="aside" paddingX={5}><GenreList onSelectGenre={(genre)=>setSelectedGenre(genre)}/></GridItem>
       </Show>
       <GridItem area="main" >
-        <GameGrid/>
+        <GameGrid selectedGenre={selectedGenre}/>
       </GridItem>
     </Grid>
   )
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,11 @@
 
 
-import { HStack, Image, List, ListItem,Spinner,Text } from '@chakra-ui/react';
-import useGenres from '../hooks/useGenres';
-
-const GenreList = () => {
+import { Button, HStack, Image, List, ListItem,Spinner,Text } from '@chakra-ui/react';
+import useGenres, { Genre } from '../hooks/useGenres';
+interface Props{
+    onSelectGenre:(genre:Genre)=>void;
+}
+const GenreList = ({onSelectGenre}:Props) => {
     const {data,isLoading,error}=useGenres();
     if (error) return null
     if (isLoading) return <Spinner/>;
@@ -13,7 +15,7 @@ const GenreList = () => {
             <HStack>
                 
                 <Image boxSize='32px' borderRadius={8} src={genre.image_background}></Image>
-                <Text fontSize='lg'>{genre.name}</Text>
+                <Button onClick={()=>onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
             </HStack>
         </ListItem>)}
     </List>
